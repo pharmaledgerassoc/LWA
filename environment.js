46eda3a6978a9a186b288f5dc7cd68010546e859
adrian.ganga <adiganga2002@gmail.com>
2023-09-07 17:13:06 +0300

    axiologic-pla/dev#515

---
+++ environment.js
@@ -0,0 +1,11 @@
+export default {
+  "epiDomain": "preqa.epi",
+  "epiProtocolVersion": 1,
+  "appBuildVersion": "test_release.1.3u",
+  "timePerCall": 10000,
+  "totalWaitTime": 60000,
+  "gto_TimePerCall": 3000,
+  "gto_TotalWaitTime": 15000,
+  "enableEpiDomain": true,
+  "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
+}
