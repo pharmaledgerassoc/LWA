2aa8bc050dfca561a4a86758dbfe963c3d18513a
adrian.ganga <adiganga2002@gmail.com>
2023-10-18 10:28:18 +0300

    add html tag + other small css fixes

--- environment.js
+++ environment.js
@@ -1,11 +1,11 @@
 export default {
-  "epiDomain": "preqa.epi",
+  "epiDomain": "local.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "2023.2.0",
+  "appBuildVersion": "2023.0.4",
   "timePerCall": 10000,
   "totalWaitTime": 60000,
   "gto_TimePerCall": 3000,
   "gto_TotalWaitTime": 15000,
   "enableEpiDomain": true,
-  "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
+  "bdnsUrl": "http://127.0.0.1:8080/bdns"
 }
