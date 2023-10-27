7f69c6b20385e5b47ca1268b0e85e2cbd48c81e6
adrian.ganga <adiganga2002@gmail.com>
2023-10-27 12:23:44 +0300

    fix environment.js and put LWA2.0.1 version

--- environment.js
+++ environment.js
@@ -1,11 +1,11 @@
 export default {
-  "epiDomain": "local.epi",
+  "epiDomain": "preqa.epi",
   "epiProtocolVersion": 1,
-  "appBuildVersion": "2023.0.4",
+  "appBuildVersion": "LWA2.0.1",
   "timePerCall": 10000,
   "totalWaitTime": 60000,
   "gto_TimePerCall": 3000,
   "gto_TotalWaitTime": 15000,
   "enableEpiDomain": true,
-  "bdnsUrl": "http://127.0.0.1:8080/bdns"
+  "bdnsUrl": "https://raw.githubusercontent.com/PharmaLedger-IMI/mobile-bdns/master/bdns.json"
 }
