65c44c3ab2f4df360991030f0be5dea56667fc78
adrian.ganga <adiganga2002@gmail.com>
2024-01-15 15:15:26 +0200

    add go back button for 404 err page

--- app/js/controllers/404ErrorController.js
+++
@@ -1,9 +0,0 @@
-import {translate} from "../translationUtils.js";
-import {setFontSize} from "../../../utils.js";
-
-window.onload = async (event) => {
-  await translate();
-  setTimeout(setFontSize, 0);
-}
-
-
