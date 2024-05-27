b2b54d90306d67563d94959a2eda05da0820f54b
daniel.rajcan <rajcan.daniel@gmail.com>
2024-05-27 11:02:22 +0200

    Update init.sh

--- terraform/init.sh
+++ terraform/init.sh
@@ -2,7 +2,7 @@
 
 terraform init -reconfigure \
   -backend-config="bucket=tf-state-${AWS_REGION}-${AWS_ACCOUNT_ID}" \
-  -backend-config="key=${NETWORK_NAME}/LWA/${FQDN}/terraform.tfstate" \
+  -backend-config="key=${TF_network}/LWA/${TF_fqdn}/terraform.tfstate" \
   -backend-config="region=${AWS_REGION}" \
   -backend-config="encrypt=true" \
   -backend-config="dynamodb_table=tf-state-${AWS_REGION}-${AWS_ACCOUNT_ID}"
