fd8a0151508374d523b91f6ef7afc2806556a9d0
vnogueira91 <vitor.nogueira@pdmfc.com>
2025-03-28 23:55:42 +0000

    TRUST-407 - attempt to fix product information

--- app/js/controllers/LeafletController.js
+++ app/js/controllers/LeafletController.js
@@ -259,7 +259,7 @@ function LeafletController() {
         try {
             if (this.selectedDocument === DocumentsTypes.INFO) {
                 this.showModal("product-modal");
-                renderProductInformation(result, this.metadata?.batchData);
+                renderProductInformation(result, this.metadata?.productData?.batchData);
                 this.loadPrintContent("product-modal");
                 return;
             }
